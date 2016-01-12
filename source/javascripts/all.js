19a37e4d2f1587bfaf478c72365b531f5738cef1
Damir Kotoric <damir.kotoric@envato.com>
2016-01-12 19:14:20 +1100

    working on the hero section, still need to fix IE support and need to generate proper .webm file

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,8 @@
 //= require_tree .
+
+// https://github.com/anselmh/object-fit
+objectFit.polyfill({
+    selector: 'video', // this can be any CSS selector
+    fittype: 'cover', // either contain, cover, fill or none
+    disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
+});
