bab464862245aca5664272041a202747536e1a0f
Damir Kotoric <damir.kotoric@envato.com>
2016-01-18 20:13:53 +1100

    added showcase and equipment sections

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,3 @@
 //= require_tree .
 
 // https://github.com/anselmh/object-fit
-objectFit.polyfill({
-    selector: 'video', // this can be any CSS selector
-    fittype: 'cover', // either contain, cover, fill or none
-    disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
-});
