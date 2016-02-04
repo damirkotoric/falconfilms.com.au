42ec0e83c77608d24fe21484f2280a0ff27e5ca4
Damir Kotoric <damir.kotoric@envato.com>
2016-02-04 23:46:57 +1100

    new custom contact form hooked up to Google forms

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -21,3 +21,58 @@ jQuery(function($) {
     }
   });
 });
+
+$(document).ready(function() {
+  // Listeners
+  $("[data-trigger-modal]").on("click", modalTrigger)
+  $(".modal__close").on("click", closeModal)
+  $("#ss-form").on("submit", submitForm)
+  $(".confirmation__confirmation-lolz").on("click", confirmationClose)
+});
+
+function modalTrigger(event) {
+  event.preventDefault()
+  $trigger = $(event.currentTarget)
+  $packageSelectBox = $("#package")
+  $("html").toggleClass("-modal-visible")
+  switch ($trigger.attr("id")) {
+    case "enquire-free":
+      $packageSelectBox.prop("selectedIndex", 0)
+      break
+    case "enquire-professional":
+      $packageSelectBox.prop("selectedIndex", 1)
+      break
+    case "enquire-bespoke":
+      $packageSelectBox.prop("selectedIndex", 2)
+      break
+  }
+  $("#email").focus()
+}
+
+function closeModal(event) {
+  event.preventDefault()
+  $("html").removeClass("-modal-visible")
+}
+
+function submitForm(event) {
+  event.preventDefault()
+  $form = $(event.currentTarget)
+  $form.find("button[type='submit']").attr("disabled", true).text("Sending...")
+  $.ajax({
+    type: 'POST',
+    url: $(this).attr('action'),
+    data: $(this).serialize(),
+    dataType: 'json',
+    error: function(json) {
+      // Because of 'Access-Control-Allow-Origin' stupidity I have to bind
+      // to the 'error' listener and hope that the request went through okay anyway.
+      $("html").removeClass("-modal-visible").addClass("-contact-form-submitted")
+      $form.find("button[type='submit']").attr("disabled", false).text("Sending Enquiry")
+      window.scrollTo(0, 0)
+    }
+  })
+}
+
+function confirmationClose(event) {
+  $("html").removeClass("-contact-form-submitted")
+}
